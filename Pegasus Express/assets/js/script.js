ccc5cd8ea93286a80398c48943112de2d4f76b67
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-08-30 18:08:33 +0700

    fixed

--- Pegasus Express/assets/js/script.js
+++ Pegasus Express/assets/js/script.js
@@ -5,7 +5,7 @@ function myFunction() {
   window.onclick = function(e) {
     if (!e.target.matches('.dropbtn')) {
     var dropdown_list = document.getElementById("dropdown-list");
-      if (dropdown+list.classList.contains('show')) {
+      if (dropdown_list.classList.contains('show')) {
         dropdown_list.classList.remove('show');
       }
     }
